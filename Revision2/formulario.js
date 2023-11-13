49e71acba4703f0ed59a20014f91da6915d36d7a
dapla365 <davidplaza03@iesamachado.org>
2023-11-13 16:18:28 +0100

    Arreglado

--- Revision2/formulario.js
+++ Revision2/formulario.js
@@ -55,7 +55,7 @@ function validar() {
         comprobaciones.push('true');
     } else {
         if (pin1_invalido) {
-            ObligaPin1.innerHTML = 'Pin necesita 8 carácteres';
+            ObligaPin1.innerHTML = 'Pin necesita 8 digitos en positivo';
         } else {
             ObligaPin1.innerHTML = 'Este campo es obligatorio';
         }
@@ -66,7 +66,7 @@ function validar() {
         comprobaciones.push('true');
     } else {
         if (pin2_invalido) {
-            ObligaPin2.innerHTML = 'Pin necesita 8 carácteres';
+            ObligaPin2.innerHTML = 'Pin necesita 8 digitos en positivo';
         } else {
             ObligaPin2.innerHTML = 'Este campo es obligatorio';
         }
@@ -106,53 +106,60 @@ function validarApellido() {
     return valido;
 }
 function validarCorreo() {
-    let validar = "";
-    return validar.test(correo.value);
+    var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    return validEmail.test(correo.value);
 }
 function validarDNI() {
     let dnia = dni.value.toUpperCase();
     var validar_dni = /^[XYZ]?\d{8}[A-Z]$/;
-    if (validar_dni.test(dnia) === true) {
+    let valida = false;
+    if (validar_dni.test(dnia)) {
         let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
 
         let numero = dnia.charAt(0) + dnia.charAt(1) + dnia.charAt(2) + dnia.charAt(3) + dnia.charAt(4) + dnia.charAt(5) + dnia.charAt(6) + dnia.charAt(7);
         let resto = numero % 23;
         let dni_correspondiente = numero + letras[resto];
-        if (dni_correspondiente === dnia) {
+
+        if (dni_correspondiente == dnia) {
             dni_invalido = false;
-            return true;
+            valida = true;
         } else {
             dni_invalido = true;
-            return false;
         }
-    } else {
-        return false;
-    }
+    } 
+    return valida;
 
 }
 function validarPin1() {
-    if (pin1.value.length === 8) {
+    let valida = false;
+    if (pin1.value.length == 8 && isNumber(pin2.value)) {
         pin1_invalido = false;
-        return true;
+        valida = true;
     } else {
         pin1_invalido = true;
-        return false;
     }
+    return valida;
 }
 function validarPin2() {
-    if (pin2.value.length === 8) {
+    let valida = false;
+    if (pin2.value.length == 8 && isNumber(pin2.value)) {
         pin2_invalido = false;
-        return true;
+        valida = true;
     } else {
         pin2_invalido = true;
-        return false;
     }
+    return valida;
 }
 function comprobarPin() {
-    if (pin1.value === pin2.value) {
-        return true;
+    let valida = false;
+    if (pin1.value == pin2.value) {
+        valida = true;
     }
-    return false;
+    return valida;
+}
+function isNumber(numero){
+    const numberPattern = /^[+]?[0-9]*$/;
+    return numberPattern.test(numero);
 }
 
 
@@ -177,7 +184,6 @@ function validarTodo() {
     for (let i = 0; i <= comprobaciones.length; i++) {
         if (comprobaciones[i] == 'false') {
             valida = false;
-            break;
         }
     }
     if (valida) {
@@ -185,18 +191,16 @@ function validarTodo() {
         let ciudad = document.getElementById("ciudad");
         let telefono = document.getElementById("telefono");
         let text = "Su usuario es: " + nombre.value[0] + nombre.value[1] + apellido.value[0] + apellido.value[1];
+        if (telefono.value != "" && telefono.value != null && telefono.value.length == 9) { 
+            text += telefono.value[6]+ telefono.value[7]+ telefono.value[8];
+        }
         if (direccion.value != "" && direccion.value != null) {
             text += "\n" + direccion.value;
         }
         if (ciudad.value != "" && ciudad.value != null) {
             text += "\n" + ciudad.value;
         }
-        if (telefono.value != "" && telefono.value != null) {
-            text += "\n" + telefono.value;
-        }
         alert(text)
         limpiarComprobaciones();
-    } else {
-
     }
 }
