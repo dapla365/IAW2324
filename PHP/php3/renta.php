29a73bf657b0ff77aa89adc3b9d791369c4d7b16
dapla365 <davidplaza03@iesamachado.org>
2023-12-09 17:13:22 +0100

    renta

--- PHP/php3/renta.php
+++ PHP/php3/renta.php
@@ -1,1 +1,60 @@
-Investiga sobre la utilidad del archivo de configuración php.ini: directivas, secciones, opciones de configuración, etc. ¿Cómo una configuración u otra puede afectar al tratamiento que PHP realiza por los datos que pudieran suministrarse por un formulario?
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Renta</title>
+</head>
+<body>
+    <h1>Renta</h1>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <ul>
+            <li>
+                <label for="nombre">Nombre:</label>
+                <input type="text" name="nombre" id="nombre" required>
+            </li>
+            <li>
+                <label for="apellido">Apellido:</label>
+                <input type="text" name="apellido" id="apellido" required>
+            </li>
+            <li>
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" required>
+            </li>
+            <li>
+                <label for="dni">DNI:</label>
+                <input type="text" name="dni" id="dni" required>
+            </li>
+            <li>
+                <label for="salario">Salario Bruto:</label>
+                <input type="number" name="salario" id="salario" required>
+            </li>
+            <li>
+                <button type="submit">Calcular</button>
+            </li>
+        </ul>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nombre = htmlspecialchars($_POST["nombre"]);
+        $apellido = htmlspecialchars($_POST["apellido"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $dni = htmlspecialchars($_POST["dni"]);
+        $salario = htmlspecialchars($_POST["salario"]);
+
+
+        $tipoImpositivo = 0.15; 
+        $impuesto = $salario * $tipoImpositivo;
+
+        echo "<h2>Resultados:</h2>";
+        echo "<p>Nombre: $nombre $apellido</p>";
+        echo "<p>Email: $email</p>";
+        echo "<p>DNI: $dni</p>";
+        echo "<p>Salario Bruto: $salario</p>";
+        echo "<p>Tipo Impositivo: " . ($tipoImpositivo * 100) . "%</p>";
+        echo "<p>Impuesto a Pagar: $impuesto</p>";
+    }
+    ?>
+</body>
+</html>
--- PHP/php3/rentareto.php
+++ PHP/php3/rentareto.php
@@ -3,20 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-
-    ?>
-</body>
-</html>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Renta</title>
 </head>
 <body>
