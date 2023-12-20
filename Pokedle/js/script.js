08a37cb52a9a9b5c957bcbdc73200d2f48eb9310
dapla365 <davidplaza03@iesamachado.org>
2023-12-20 17:50:50 +0100

    generacion

--- Pokedle/js/script.js
+++ Pokedle/js/script.js
@@ -6,6 +6,7 @@ document.getElementById("dark_button").onclick = function () {
     document.getElementById("names").classList.add('dark');
     document.getElementById("pokemon_table").classList.add('dark');
     document.getElementById("pokemon_select").classList.add('dark');
+    document.getElementById("gens").classList.add('dark');
 }
 document.getElementById("light_button").onclick = function () {
     document.getElementById("dark_button").classList.remove('invisible');
@@ -15,7 +16,9 @@ document.getElementById("light_button").onclick = function () {
     document.getElementById("names").classList.remove('dark');
     document.getElementById("pokemon_table").classList.remove('dark');
     document.getElementById("pokemon_select").classList.remove('dark');
+    document.getElementById("gens").classList.remove('dark');
 }
+
 
 $(document).keyup(function (event) {
     if ($("#valor").is(":focus") && event.key == "Enter") {
@@ -26,9 +29,39 @@ $(document).keyup(function (event) {
 $(function () {
     $("body").click(function (e) {
         if (e.target.id == "valor") {
-            document.getElementById("pokemon_select").classList.remove('invisible');
+            document.getElementById("pokemon_select").classList.add('playing');
         } else {
-            document.getElementById("pokemon_select").classList.add('invisible');
+            document.getElementById("pokemon_select").classList.remove('playing');
+        }
+
+        for(let i=1;i<=8;i++){
+            if($(`#gen${i}:checked`).length > 0)document.getElementById(`gen${i}_label`).classList.add('nogray');
+            else document.getElementById(`gen${i}_label`).classList.remove('nogray');
+        }
+    });
+
+    $("#play").click(function (e) {
+        let comprobacion = false;
+        let gens = [];
+        for(let i=1;i<=8;i++){
+            if($(`#gen${i}:checked`).length > 0) {
+                comprobacion = true;
+                gens.push(i);
+            }
+        }
+        if(comprobacion){
+            document.getElementById("play").classList.add('playing');
+            document.getElementById("valor").classList.add('playing');
+            document.getElementById("pokemon_select").classList.add('playing');
+            document.getElementById("names").classList.add('playing');
+            document.getElementById("pokemon_table").classList.add('playing');            
+        } else{
+            document.getElementById("play").classList.remove('playing');
+            document.getElementById("valor").classList.remove('playing');
+            document.getElementById("pokemon_select").classList.remove('playing');
+            document.getElementById("names").classList.remove('playing');
+            document.getElementById("pokemon_table").classList.remove('playing');            
         }
     });
 })
+                                                       
