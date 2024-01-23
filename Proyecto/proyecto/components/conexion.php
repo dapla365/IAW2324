1a508169ab15f40bf9d459021f758f848dbf4030
dapla365 <davidplaza03@iesamachado.org>
2024-01-23 21:25:38 +0100

    conexion bd

--- Proyecto/proyecto/components/conexion.php
+++ Proyecto/proyecto/components/conexion.php
@@ -2,7 +2,7 @@
 $hostname = "sql204.thsite.top";
 $username = "thsi_35476428";
 $password = "*******";
-$dbname = "thsi_35476428_pruebas";
+$dbname = "thsi_35476428_proyecto";
 
 // Crear conexión
 $mysqli = new mysqli($hostname,$username,$password,$dbname);
