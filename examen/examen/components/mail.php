60121f6cd4cf729d9741ccc6235a7be95a9dae4b
dapla365 <davidplaza03@iesamachado.org>
2024-02-21 00:10:18 +0100

    mail php

--- examen/examen/components/mail.php
+++ examen/examen/components/mail.php
@@ -1,8 +1,8 @@
 <?php 
-require "PHPMailer/Exception.php";
-require "PHPMailer/PHPMailer.php";
-require "PHPMailer/SMTP.php";
- 
+require "../PHPMailer/src/Exception.php";
+require "../PHPMailer/src/PHPMailer.php";
+require "../PHPMailer/src/SMTP.php";
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
  
@@ -13,10 +13,10 @@ $mail->Port=465;
 $mail->SMTPDebug = "0";
 $mail->SMTPSecure = "ssl";
 $mail->SMTPAuth=true;
-$mail->Username="tucorreo@gmail.com";
-$mail->Password="Tu contraseña aplicativa";
-$mail->setFrom("tucorreo@gmail.com","David Plaza");
-$mail->addAddress("destinatario@gmail.com","Pepito2");
+$mail->Username="davidplaza03@iesamachado.org";
+$mail->Password="*********";
+$mail->setFrom("davidplaza03@iesamachado.org","David Plaza");
+$mail->addAddress("plazadiazdavid@gmail.com","David");
 $mail->Subject="OLEEEE";
 $mail->msgHTML("Hola soy un mensaje");
  
